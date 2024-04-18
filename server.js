f9bd8724317081ea3e2f4c90472de89122637444
Catherinen29 <cat_nevin@hotmail.com>
2024-04-18 12:20:48 +0100

    Set up openai call.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const {BOT_TOKEN} = require("./config.js");
-const {openAI} = require("openai");
+const {BOT_TOKEN, openAI_KEY} = require("./config.js");
+const {OpenAI} = require("openai");
+const API_URL = "https://api.openai.com/v1/chat/completions"
 const {Client, GatewayIntentBits} = require("discord.js");
+const { Models } = require("openai/resources/models.js");
 
 const BOT = new Client(
     {intents: [GatewayIntentBits.Guilds,
@@ -9,14 +11,33 @@ const BOT = new Client(
     GatewayIntentBits.GuildMembers,]}
 );
 
+const myClient = new OpenAI({apiKey: openAI_KEY})
+
 BOT.once("ready", (response) => {console.log("my bot is online")});
 
 BOT.on('messageCreate', (msg) => {
-    console.log('user message: ', msg.content)
     
     if (msg.author !== BOT.user) {
-        msg.channel.send('howdy!')
-}
+        console.log('user message: ', msg.content)
+        // console.log('myClient ', myClient)
+
+        let AI_res = myClient.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages: [
+                {"role": "system", 
+                "content": "You are a helpful assistant."},
+                {"role": "user",
+                "content": msg.content}, 
+                {"role": "assistant",
+                "content": "Hi there!"}
+            ]}
+        )
+ 
+        AI_res.then(data => {
+            // console.log('AI RESPONSE: ', data.choices[0].message.content)
+            msg.reply(data.choices[0].message.content)
+        })
+    }
 })
 
 BOT.login(BOT_TOKEN);
