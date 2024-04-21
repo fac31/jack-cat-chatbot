f0d3e59fd7f52d4bc84531ae344416ac073c1b34
Catherinen29 <cat_nevin@hotmail.com>
2024-04-21 17:02:04 +0100

    Interacting with buttons.

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,4 +1,8 @@
-const { SlashCommandBuilder } = require('discord.js')
+const { ButtonBuilder, 
+    ButtonStyle, 
+    SlashCommandBuilder, 
+    PermissionFlagsBits, 
+    ActionRowBuilder } = require('discord.js')
 const wait = require('node:timers/promises').setTimeout;
 
 
@@ -10,10 +14,58 @@ module.exports = {
         .setName('ping')
         .setDescription('Replies with Pong!'),
 
-        async execute (interaction) {
-			// ephemeral message means only the executor of the slash command can see the response. 
-            await interaction.reply({content: 'secret pong!', ephemeral: true});
-            await interaction.followUp('Pong again!');
+        async execute (interaction) {		
+
+        const confirm = new ButtonBuilder()
+            .setCustomId('confirm')
+            .setLabel('Confirm Pong')
+            .setStyle(ButtonStyle.Danger);
+
+		const cancel = new ButtonBuilder()
+			.setCustomId('cancel')
+			.setLabel('Cancel Pong')
+			.setStyle(ButtonStyle.Secondary);
+        
+        const row = new ActionRowBuilder().addComponents(cancel, confirm);
+
+        const response = await interaction.reply({
+            content: `Are you sure you want to play?`,
+            components: [row]
+        })
+        console.log('interaction.user.id', interaction.user.username)
+
+        const collectorFilter = b => b.user.id === interaction.user.id
+
+        try {
+            const confirmation = await response.awaitMessageComponent({ 
+                filter: collectorFilter,
+                time: 60_000
+            })
+        
+            console.log('confirmation.customId ', confirmation.customId)
+
+            if (confirmation.customId === 'confirm') {
+                await interaction.editReply('Pong!')
+                console.log('edit to Pong!:');
+            } else if (confirmation.customId === 'cancel') {
+                await interaction.editReply('Game cancelled.')
+            }
+        } catch (e) {
+            await interaction.editReply({ 
+                content: 'No response received within one minute. Cancelling.',
+                components: [] 
+            })
+        }
+
+
+		// await interaction.reply({
+		// 	content: `confirmed pong!`,
+		// 	components: [row],
+		// });
+
+        // ephemeral message means only the executor of the slash command can see the response. 
+        // await interaction.reply({content: 'secret pong!', ephemeral: true});
+        // await interaction.followUp('Pong again!');
 
 
             // Wait 2seconds and change the reply
