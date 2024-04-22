5299c9c4b348a413536ec4494b8d0a7d7e2965fe
Catherinen29 <cat_nevin@hotmail.com>
2024-04-22 19:29:47 +0100

    Successfully implement AI responses to user messages.

--- events/AImessageCreate.js
+++ events/AImessageCreate.js
@@ -1,57 +1,72 @@
 const { Events } = require('discord.js');
 const {OpenAI} = require("openai");
+const {openAI_KEY} = require("../config.js")
 
 
-// module.exports = {
-//     name: Events.MessageCreate,
-//     async execute(interaction) {
+module.exports = {
+    name: Events.MessageCreate,
 
-//         const myClient = new OpenAI({apiKey: openAI_KEY})
+    async execute(interaction) {
 
-//         if (interaction.author !== BOT.user) {
-//             // console.log('interaction content ', interaction)
-//             console.log('user message: ', interaction.content)
-            
-//             // Check if bot is mentioned in message
-//             if (interaction.content.includes(`@1227708046810284172`) 
-//             || 
-//             // Or if it is a reply to a chatbot message
-//             (interaction.channel.messages.cache.get(interaction.reference.messageId).author.id === `1227708046810284172`)
-//                 ) {
-    
-    
-//                 let AI_res = myClient.chat.completions.create({
-//                     model: "gpt-3.5-turbo",
-//                     messages: [
-//                         {"role": "system", 
-//                         "content": "You are a helpful assistant. You should reply in a sarcastic manner."},
-//                         {"role": "user",
-//                         "content": interaction.content}, 
-//                         {"role": "assistant",
-//                         "content": "Hi there!"}
-//                     ]}
-//                 )
+        const myClient = new OpenAI({apiKey: openAI_KEY})
         
-//                 AI_res.then(data => {
-//                     console.log('AI RESPONSE: ', data.choices[0].message)
-//                     // console.log('interaction content', interaction.reference.messageId)
-//                     msg.reply(data.choices[0].message.content)
-    
-    
-//                 })
-//                 .then((r) => {
-//                     if (!r.ok) throw new Error(`Error in AI call! Error: ${r.status}`)
-//                     return r.json()
-//                 })
-//                 .catch(err => console.log('catch error ', err))
-//             }
-    
-//         }
+        let AI_res = myClient.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages: [
+                {"role": "system", 
+                "content": "You are a helpful assistant. You should reply in a sarcastic manner."},
+                {"role": "user",
+                "content": interaction.content}, 
+                {"role": "assistant",
+                "content": "Hi there!"}
+            ]}
+        )
+        // console.log('interaction.author.id', interaction.author.id)
 
-//     }
-// }
-// BOT.on('messageCreate', (msg) => {
-    
+        if (interaction.author.id !== `1231183898906726430`) {
+            // console.log('interaction ', interaction)
+            // console.log('user message/interaction.content', interaction.content)
+            
+            // Check if bot is mentioned in message
+            if (interaction.content.includes(`1231183898906726430`)) {
 
-// }
-// )
+                // console.log('interaction.reference ', interaction.channel.messages.cache.get(interaction.reference.messageId).author.id)
+                console.log('INSIDE THE tag BLOCK ******',)
+                    // console.log('TAG interaction content', interaction.reference.messageId)
+
+                AI_res.then(data => {
+                    // console.log('AI RESPONSE: ', data.choices[0].message)
+                    interaction.reply(data.choices[0].message.content)
+                    // return data;
+                })
+                // .then((r) => {
+                    // console.log('r', r)
+                //     if (!r.ok) throw new Error(`Error in AI call! Error: ${r.status}`)
+                //     return r.json()
+                // })
+                .catch(err => console.log('catch error ', err))
+
+            } else if (
+                interaction.channel.messages.cache.get(interaction.reference) &&
+                interaction.channel.messages.cache.get(interaction.reference.messageId).author.id === `1231183898906726430`) {
+            // Or if it is a reply to a chatbot message
+                AI_res.then(data => {
+                    // console.log('AI RESPONSE: ', data.choices[0].message)
+                    // console.log('interaction content', interaction.reference.messageId)
+                    interaction.reply(data.choices[0].message.content)
+                })
+                .then((r) => {
+                    if (!r.ok) throw new Error(`Error in AI call! Error: ${r.status}`)
+                    return r.json()
+                })
+                .catch(err => console.log('catch error ', err))
+
+            } else {
+                console.log('******** no response necessary ********',)
+                return;
+            }
+    
+        }
+
+    }
+}
